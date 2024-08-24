802e6c81f08127d059a54af597dff9726479c156
Nadeem13 <ahangarnadeem786.com>
2024-08-24 18:14:01 +0530

    backend.tsx added in src/services

--- src/screens/Home/StudentDetails/index.tsx
+++ src/screens/Home/StudentDetails/index.tsx
@@ -1,19 +1,19 @@
-import {View, Text} from 'react-native';
-import React, {ReactElement} from 'react';
+import { View, Text } from 'react-native';
+import React, { ReactElement } from 'react';
 import CustomSwitch from '../../../common/components/customSwitch';
 import CustomHeader from '../../../common/components/customHeader';
-import {IMAGES} from '../../../common/constants/images';
-import {goBack} from '../../../common/utils/navigatorUtils';
+import { IMAGES } from '../../../common/constants/images';
+import { goBack } from '../../../common/utils/navigatorUtils';
 import CustomCheckbox from '../../../common/components/customCheckbox';
 import PageSkelton from '../../../common/components/pageSkelton';
 import Spacer from '../../../common/components/utility/spacer';
-import {ParamListBase, RouteProp} from '@react-navigation/native';
+import { ParamListBase, RouteProp } from '@react-navigation/native';
 import useStudentController from './SutdentDetailsController';
 
 const tabsData = [
-  {id: 1, title: 'Daily'},
-  {id: 2, title: 'Weakly'},
-  {id: 3, title: 'Daras/Exam'},
+  { id: 1, title: 'Daily' },
+  { id: 2, title: 'Weakly' },
+  { id: 3, title: 'Daras/Exam' },
 ];
 
 type StudentDetailsParams = RouteProp<ParamListBase, 'HomeScreen'>;
@@ -21,13 +21,13 @@ interface Props {
   route: StudentDetailsParams;
 }
 const StudentDetails = (props: Props): ReactElement => {
-  const {route} = props;
+  const { route } = props;
   const routeParams = route.params;
-  const {userRef} = useStudentController(routeParams);
+  const { userRef } = useStudentController(routeParams);
 
   console.log(routeParams);
   return (
-    <View style={{width: '100%', height: '100%'}}>
+    <View style={{ width: '100%', height: '100%' }}>
       <CustomHeader
         leftAccessories={IMAGES.BACK}
         titlle={userRef.current.name}
@@ -36,9 +36,13 @@ const StudentDetails = (props: Props): ReactElement => {
       />
       <Spacer height={10} />
       <PageSkelton>
-        <CustomSwitch switchData={tabsData} />
+        <CustomSwitch switchData={tabsData} onTabChange={(index, title) => {
+          console.log(index + " " + title);
+        }} />
         <Spacer height={20} />
-        <CustomCheckbox label="Abc" onValueChange={() => {}} />
+        <CustomCheckbox label="Abc" onValueChange={(isChecked) => {
+          console.log(isChecked);
+        }} />
       </PageSkelton>
     </View>
   );
